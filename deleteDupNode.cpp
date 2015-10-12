600f585863a6aada8b333559bcd17503ebb5eee0
Riya-Gao <wenjungaoat@gmail.com>
2015-10-12 14:01:25 -0400

    This version will change the orignal list.

---
+++ deleteDupNode.cpp
@@ -0,0 +1,22 @@
+ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = new ListNode(0);
+        ListNode* returnval = temp;
+        temp->next = NULL;
+        while (head){
+            int value = head->val;
+            if (head->next && head->next->val == value){
+                while(head && head->next && head->next->val == value){
+                    head = head->next;
+                }
+                if (head){
+                    head = head->next;
+                }
+            else{
+                temp->next = head;
+                head = head->next;
+                temp = temp->next;
+                temp->next = NULL;
+            }
+        }
+        return returnval->next;
+    }
